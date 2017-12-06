806b829c67a6cedd857888a5ad3b1db4af42dd6b
rickytranmer <rwtranmer@gmail.com>
2017-12-06 16:56:37 -0700

    denies click if already checked

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
+var numMoves = 0;
+
+
 $(function(){
 	console.log('document ready');
 	$('td').click(function(){
 		console.log($(this).closest('td'));
-		$(this).html("O");
+		if ((numMoves % 2 == 0) && (!($(this).closest('td').hasClass('checked')))) {
+			$(this).html("X").addClass('checked');
+			numMoves++;
+		} else if ((numMoves % 2 == 1) && (!($(this).closest('td').hasClass('checked')))) {
+			$(this).html("O").addClass('checked');
+			numMoves++;
+		}
 	});
 });
