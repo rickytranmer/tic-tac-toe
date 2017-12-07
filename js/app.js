a4d81b3797c8611e25e337700bb9f24f4b72ac6d
rickytranmer <rwtranmer@gmail.com>
2017-12-06 17:23:26 -0700

    add reset button (no functionality yet)

--- js/app.js
+++ js/app.js
@@ -1,19 +1,22 @@
 var numMoves = 0;
 
+
+// - Fill selected cell (if not already checked)
 function checkCell ($clickedCell) {
 	if ((numMoves % 2 == 0) && (!($clickedCell.hasClass('checked')))) {
 		$clickedCell.html("X").addClass('checked').css('background', 'pink');
 		numMoves++;
+		console.log('move: ' + numMoves + '-X');
 	} else if ((numMoves % 2 == 1) && (!($clickedCell.hasClass('checked')))) {
 		$clickedCell.html("O").addClass('checked').css('background', 'lightBlue');
 		numMoves++;
+		console.log('move: ' + numMoves + '-O');
 	}
 }
 
 $(function(){
 	console.log('document ready');
 	$('td').click(function(){
-		// - Select clicked cell, deny if already checked
 		console.log($(this).closest('td'));
 		
 		checkCell($(this).closest('td'));
@@ -22,4 +25,6 @@ $(function(){
 
 		//ADD - Check for win condition
 	});
+
+	//ADD - Reset button
 });
