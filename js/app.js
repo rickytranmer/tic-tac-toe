11c8b361b7077b8f144c9e5edf3cb7cd8211491f
rickytranmer <rwtranmer@gmail.com>
2017-12-06 17:07:52 -0700

    moves act of checking to function

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
 var numMoves = 0;
 
+function checkCell ($clickedCell) {
+	if ((numMoves % 2 == 0) && (!($clickedCell.hasClass('checked')))) {
+		$clickedCell.html("X").addClass('checked').css('background', 'pink');
+		numMoves++;
+	} else if ((numMoves % 2 == 1) && (!($clickedCell.hasClass('checked')))) {
+		$clickedCell.html("O").addClass('checked').css('background', 'lightBlue');
+		numMoves++;
+	}
+}
+
 $(function(){
 	console.log('document ready');
 	$('td').click(function(){
 		// - Select clicked cell, deny if already checked
 		console.log($(this).closest('td'));
-		if ((numMoves % 2 == 0) && (!($(this).closest('td').hasClass('checked')))) {
-			$(this).html("X").addClass('checked').css('background', 'pink');
-			numMoves++;
-		} else if ((numMoves % 2 == 1) && (!($(this).closest('td').hasClass('checked')))) {
-			$(this).html("O").addClass('checked').css('background', 'lightBlue');
-			numMoves++;
-		}
+		
+		checkCell($(this).closest('td'));
 
+		//ADD - Cat's game if numMoves >= 9 (8?)
 
 		//ADD - Check for win condition
 	});
