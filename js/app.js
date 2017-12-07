33d2a3c23bbeaff85d739822de8fc20aa6a0cc84
rickytranmer <rwtranmer@gmail.com>
2017-12-06 17:01:16 -0700

    adds color to checked boxes

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 var numMoves = 0;
 
-
 $(function(){
 	console.log('document ready');
 	$('td').click(function(){
+		// - Select clicked cell, deny if already checked
 		console.log($(this).closest('td'));
 		if ((numMoves % 2 == 0) && (!($(this).closest('td').hasClass('checked')))) {
-			$(this).html("X").addClass('checked');
+			$(this).html("X").addClass('checked').css('background', 'pink');
 			numMoves++;
 		} else if ((numMoves % 2 == 1) && (!($(this).closest('td').hasClass('checked')))) {
-			$(this).html("O").addClass('checked');
+			$(this).html("O").addClass('checked').css('background', 'lightBlue');
 			numMoves++;
 		}
+
+
+		//ADD - Check for win condition
 	});
 });
